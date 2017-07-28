514899156d87fa18d3879b8716681745209eccba
shashwat shikshu <shashwat.shikshu@gmail.com>
2017-07-28 14:00:28 +0530

    fixed linting

--- src/+profile/my-account.page.tsx
+++ src/+profile/my-account.page.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
 import { inject, observer } from 'mobx-react'
+import * as React from 'react'
 
 import { PageTitle } from '../components'
 import { AccountStore } from '../store/account.store'
